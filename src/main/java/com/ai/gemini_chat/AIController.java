e4367154f0e3525046e0e45fcfc3bb3b108eb680
SurajGiri55446 <surajgiri@55446gmail.com>
2025-03-16 23:40:07 +0530

    Updated project with latest changes

--- src/main/java/com/ai/gemini_chat/AIController.java
+++ src/main/java/com/ai/gemini_chat/AIController.java
@@ -1,6 +1,7 @@
 package com.ai.gemini_chat;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,5 +25,9 @@ public class AIController {
         String question = payload.get("question");
         String answer = qnAService.getAnswer(question);
         return ResponseEntity.ok(answer);
+    }
+    @GetMapping("/")
+    public String home() {
+        return "Welcome to the Chatbot API!";
     }
 }
